8126cceb7c211eb56ad414da5101c6ec543496af
Chung Eun Chae <JennyChae@MacBook-Pro-4.local>
2019-03-18 04:56:06 -0400

    first commit

--- _.js
+++ _.js
@@ -1,5 +1,34 @@
 
 
+const _ = {
+  clamp(curr,lower,upper){
+  	let num = Math.max(curr,lower);
+  	num = Math.min(num, upper);
+  	return num;
+	},
+  inRange(num,start,end){
+    if (typeof end==='undefined'){
+      end = start;
+      start = 0;
+    }
+    if (start > end){
+      let orgstart = start;
+      start=end;
+      end=orgstart;
+    }
+    return ((num >= start)&&(num < end));
+  },
+  words(str){
+    const answer = str.split(' ');
+    return answer;
+  }
+
+
+}
+
+
+
+
 
 
 
