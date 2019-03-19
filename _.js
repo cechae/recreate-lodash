7de25f35d3d6ca48347cb8e61a2c630b7a23bc0f
Chung Eun Chae <JennyChae@MacBook-Pro-4.local>
2019-03-19 16:29:28 -0400

    implemented all functionalities

--- _.js
+++ _.js
@@ -21,10 +21,71 @@ const _ = {
   words(str){
     const answer = str.split(' ');
     return answer;
+  },
+  pad(string, length){
+    if (length <= string.length){
+      return string;
+    }
+    let startPaddingLength = Math.floor((length - string.length)/2);
+    let endPaddingLength = length - string.length - startPaddingLength;
+    const spaceChar = ' ';
+    let paddedString = spaceChar.repeat(startPaddingLength)+string+spaceChar.repeat(endPaddingLength);
+    return paddedString;
+  },
+  has(object, key){
+    return object.hasOwnProperty(key);
+  },
+  invert(object){
+    let invertedObject = [];
+    for (let key in object) {
+      let originalValue = object[key];
+      invertedObject.originalValue = key;
+    }
+    return invertedObject;
+  },
+  findKey(object, predicate){
+    for (let key in object){
+      let value = object[key];
+      let predicateReturnValue = predicate(value);
+      if (predicateReturnValue){
+        return key;
+      }
+    }
+    undefined
+    return undefined;
+  },
+  drop(array, n){
+    if (n === undefined){
+      n = 1;
+    }
+    let droppedArray = array.slice(n);
+    return droppedArray;
+  },
+  dropWhile(array, predicate){
+    let index = array.findIndex((element,index)=> {
+      return !predicate(element, index);
+    });
+    return this.drop(array, index);
+  },
+  chunk(array, num){
+    if (num === undefined){
+      num = 1;
+    }
+    if (num > array.length){
+      return array; // if num of elements exceed the size of array.
+    }
+    let answerArray = [];
+    for (let i = 0; i < array.length; i+=num){
+      let arrayChunk = array.slice(i, i+num);
+      answerArray.push(arrayChunk);
+    }
+    return answerArray;
+
   }
 
 
 }
+
 
 
 
